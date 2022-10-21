541d2db9ba290a02ae0ef087b8f586b551c18ce6
AkilaRagupathy <114809477+AkilaRagupathy@users.noreply.github.com>
2022-10-21 13:33:37 +0530

    Add files via upload

---
+++ Week3Day2Assignments/system/Desktop.java
@@ -0,0 +1,22 @@
+package org.system;
+
+public class Desktop extends Computer {
+
+	
+	public void desktopSize() {
+		
+		System.out.println("The desktop Size is default");
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Desktop d=new Desktop();
+		d.computerModel();
+		d.desktopSize();
+	
+
+	}
+
+}
