42e9ada508edc86bb3958a883cf3ea99599e7498
AkilaRagupathy <114809477+AkilaRagupathy@users.noreply.github.com>
2022-10-21 13:30:17 +0530

    Add files via upload

---
+++ Week3Day2Assignments/college/College.java
@@ -0,0 +1,23 @@
+package org.college;
+
+public class College {
+
+	
+	public void collegeName()
+	
+	{
+		System.out.println("college name is Sastra");
+		
+	}
+	
+	public void collegeCode()
+	{
+		System.out.println("College code :123");
+	}
+	
+	
+	public void collegeRank()
+	{
+		System.out.println("rank is 10");
+	}
+}
